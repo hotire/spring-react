9d7d6f4ba673d5f76825b024704e2fe9a951bf73
hotire <gngh0101@gmail.com>
2022-08-25 13:29:21 +0900

    Simplify

--- src/main/kotlin/com/github/hotire/springreact/Application.kt
+++ src/main/kotlin/com/github/hotire/springreact/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringReactApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringReactApplication>(*args)
+    runApplication<Application>(*args)
 }
