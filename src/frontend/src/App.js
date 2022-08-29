8e90dce33cd81abef426afebeebcb7f1c9a602fc
hotire <gngh0101@gmail.com>
2022-08-29 15:45:43 +0900

    Add tailwind example

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div className="black-nav">블로그</div>
     </div>
   );
